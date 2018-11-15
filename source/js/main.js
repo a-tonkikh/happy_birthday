ef3977eb1b44735758d0cc14467f0ab350096cd5
a-tonkikh <tonkikhalina@yahoo.com>
2018-11-15 13:15:55 +0300

    first

---
+++ source/js/main.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+  $('.photos__list').slick({
+    infinite: true,
+    autoplay: true,
+    autoplaySpeed: 1000,
+    slidesToShow: 1,
+    slidesToScroll: 1
+  });
+});
